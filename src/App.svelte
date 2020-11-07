75ecec4bdc0175bd9d58fa98f08aff7d9001230c
Juan Villacorta <juanvillacortac@gmail.com>
2020-11-07 10:59:45 -0400

    Snipcart removed

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import './sw/sw'
   import { isOffline, updateAvailable } from './sw/store'
-  import { count, loaded } from './stores/snipcart'
 
   import { Router } from '@roxi/routify/runtime'
   import { routes } from '../.routify/routes'
@@ -47,24 +46,12 @@
   onMount(() => {
     window.addEventListener('online', handleNetworkChange)
     window.addEventListener('offline', handleNetworkChange)
-    document.addEventListener('snipcart.ready', () => {
-      window.Snipcart.events.on('snipcart.initialized', (snipcartState) => {
-        $loaded = true
-      })
-      window.Snipcart.store.subscribe(() => {
-        $count = window.Snipcart.store.getState().cart.items.count
-      })
-    })
   })
 
   $: if ($isOffline != undefined) {
     handleNetworkChange()
   }
 </script>
-
-<svelte:head>
-  <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.24/default/snipcart.css" />
-</svelte:head>
 
 <OfflineBanner/>
 <BannerUpdate/>
