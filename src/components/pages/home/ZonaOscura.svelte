01701de2fa2a19360a97c2942d64fc015f57b96c
Juan Villacorta <juanvillacortac@gmail.com>
2020-11-12 11:52:08 -0400

    Snipcart removed

--- src/components/pages/home/ZonaOscura.svelte
+++ src/components/pages/home/ZonaOscura.svelte
@@ -6,6 +6,7 @@
 
 <script>
   import { onDestroy } from 'svelte'
+  import { fade } from 'svelte/transition'
   import Viewport from '../../IntersectingViewport.svelte'
   import Wave from '../../motion/Wave.svelte'
 
@@ -35,16 +36,32 @@
       })
     }
   >
-    <Viewport bind:intersecting={$inZoneMask}>
-      <div class="h-screen text-white content-lg animate inZone">
-        <h2 class="h-16 mb-6 text-4xl font-bold leading-none font-title">Aprende con profesionales<br>disruptivos y de alto impacto</h2>
-      </div>
-      <div class="min-h-screen bg-black">
-        <div class="transform rotate-180">
-          <Wave color="#3E3E3E" />
+    <Viewport
+      bind:intersecting={$inZoneMask}
+      class="relative h-screen"
+    >
+      {#if $inZone}
+        <div class="absolute w-full h-full" in:fade|local={{delay: 800, duration: 10}} style="background: #3E3E3E">
+        </div>
+      {/if}
+      <div class="absolute flex w-full h-full animate inZone">
+        <div class="flex flex-col w-full h-full text-white content-lg">
+          <h2 class="h-16 mb-6 text-4xl font-bold leading-none font-title">Aprende con profesionales<br>disruptivos y de alto impacto</h2>
         </div>
       </div>
     </Viewport>
+    <div class="bg-black">
+      <div class="transform rotate-180">
+        <Wave color="#3E3E3E" time="15s"/>
+      </div>
+    </div>
+    <div class="min-h-screen bg-black">
+    </div>
+    <div class="bg-white">
+      <div class="transform rotate-180">
+        <Wave color="#000" time="15s"/>
+      </div>
+    </div>
   </Viewport>
 </div>
 
