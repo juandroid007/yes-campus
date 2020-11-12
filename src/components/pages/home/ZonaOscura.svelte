741fb8baf586c1aba501b6cba8b6410288bc4b11
Juan Villacorta <juanvillacortac@gmail.com>
2020-11-12 14:32:47 -0400

    Snipcart removed

--- src/components/pages/home/ZonaOscura.svelte
+++ src/components/pages/home/ZonaOscura.svelte
@@ -25,6 +25,11 @@
   }
 </style>
 
+<div class="flex flex-col items-center w-full leading-none">
+  <span class="text-6xl i jam:chevron-down animate-bounce duration-300" class:opacity-0={$inZoneMask}></span>
+  <span class="-mt-12 text-6xl i jam:chevron-down animate-bounce duration-300" class:opacity-0={$inZoneMask}></span>
+</div>
+
 <div class="mt-48 mb-4">
   <Viewport
     bind:intersecting={$inZone}
@@ -38,29 +43,26 @@
   >
     <Viewport
       bind:intersecting={$inZoneMask}
-      class="relative h-screen"
     >
-      {#if $inZone}
-        <div class="absolute w-full h-full" in:fade|local={{delay: 800, duration: 10}} style="background: #3E3E3E">
-        </div>
-      {/if}
-      <div class="absolute flex w-full h-full animate inZone">
-        <div class="flex flex-col w-full h-full text-white content-lg">
-          <h2 class="h-16 mb-6 text-4xl font-bold leading-none font-title">Aprende con profesionales<br>disruptivos y de alto impacto</h2>
+      <div class="relative h-screen">
+        {#if $inZone}
+          <div class="absolute w-full h-full" in:fade|local={{delay: 800, duration: 10}} style="background: #3E3E3E">
+          </div>
+        {/if}
+        <div class="absolute flex w-full h-full animate inZone">
+          <div class="flex flex-col w-full h-full text-white content-lg">
+            <h2 class="h-16 mb-6 text-4xl font-bold leading-none font-title">Aprende con profesionales<br>disruptivos y de alto impacto</h2>
+          </div>
         </div>
       </div>
-    </Viewport>
-    <div class="bg-black">
-      <div class="transform rotate-180">
-        <Wave color="#3E3E3E" time="15s"/>
-      </div>
-    </div>
-    <div class="min-h-screen bg-black">
-    </div>
-    <div class="bg-white">
-      <div class="transform rotate-180">
+      <div style="background: #3E3E3E">
         <Wave color="#000" time="15s"/>
       </div>
+    </Viewport>
+    <div class="min-h-screen bg-black">
+    </div>
+    <div class="transform rotate-180">
+      <Wave color="#000" time="15s"/>
     </div>
   </Viewport>
 </div>
