688e9bf8865f7584f561601d8cfbb4ad38acfa33
Juan Villacorta <juanvillacortac@gmail.com>
2020-11-16 17:58:46 -0400

    I'm the best

--- src/components/pages/home/Testimonio.svelte
+++ src/components/pages/home/Testimonio.svelte
@@ -1,37 +1,96 @@
 <script>
+  import Glide from '@glidejs/glide'
   import V from '../../IntersectingViewport.svelte'
+
+  let glide
+
+  const mountGlide = node => {
+    glide = new Glide(node, {
+      type: 'carousel',
+      perView: 1,
+      hoverpause: false,
+    })
+    glide.mount()
+  }
+
+  const testimonios = [
+    {
+      imagen: 'testimonio',
+      cita: 'El apoyo de aprendo y emprendo, en especial de Mauricio, ha sido clave en la manera de entender mi negocio de una forma integral. Me han aportado una perspectiva más amplia y eficiente. También hemos compartido como aliados estratégicos y ha sido un placer trabajar con un equipo tan capaz, innovador, estratégico, en donde la excelencia y la amistad son parte de los encuentros.',
+      autor: 'Lucía Galota - CEO de Planning RH',
+    },
+    {
+      imagen: 'juan-camilo',
+      cita: 'Aprendo y Emprendo ha sido una gran familia para el crecimiento de mi negocio, acompañándonos desde la creación de nuestra marca Trapiche Cacao hasta la exclente empresa que somos hoy en día. Sin duda alguna programas como el Youn Entrepreneur, y el Cazón Tank  han sido de apoyo fundamental para el crecimiento emprendedor de jóvenes a lo largo del territorio nacional ayudando a transformar ideas e iniciativas a negocios, fundaciones y marcas con un norte y un crecimiento. Desde el estado Táchira, Venezuela un gran Saludo de la familia Tapiche Cacao y mi persona Juan Camilo Restrepo Malavé apoyado este nuevo proyecto Yes Campus.',
+      autor: 'Juan Camilo Restrepo - Trapiche Cacao',
+    },
+  ]
 </script>
 
 <style>
-  .a1, .a2 {
-    transition: all 0.8s;
+  .glide__bullets {
+    margin-top: 2em;
+    position: initial;
+    z-index: 2;
+    display: inline-flex;
+    list-style: none;
+    transform: translateX(0);
   }
 
-  .a1:not(.intersecting) {
-    opacity: 0;
-    transform: translateX(-5%);
+  .glide__bullet {
+    width: 12px;
+    height: 12px;
+    background-color: rgba(96, 95, 95, 0.25);
+  }
+  .glide__bullet:hover, .glide__bullet:focus {
+    border: 2px solid #605F5F;
+    background-color: rgba(96, 95, 95, 0.25);
   }
 
-  .a2:not(.intersecting) {
-    opacity: 0;
-    transform: translateX(5%);
+  :global(.testimonios-slider .glide__bullet--active) {
+    background-color: #605F5F;
   }
 </style>
 
-<V class="flex flex-wrap items-center w-full min-h-screen py-12 -m-12 text-xl content-lg font-title" let:intersecting once>
-  <div class="w-full h-full p-12 lg:w-1/2">
-    <div class="flex flex-col w-full h-full a1" class:intersecting>
-      <p class="text-lg italic font-light">
-      "El apoyo de aprendo y emprendo, en especial de Mauricio, ha sido clave en la manera de entender mi negocio de una forma integral. Me han aportado una perspectiva más amplia y eficiente. También hemos compartido como aliados estratégicos y ha sido un placer trabajar con un equipo tan capaz, innovador, estratégico, en donde la excelencia y la amistad son parte de los encuentros."
-      </p>
-      <p class="mt-6 font-bold">
-      Maria Perez - Fundadora de Panasteleria
-      </p>
+<div class="py-12 testimonios-slider content-lg">
+  <V class="w-full my-auto" on:top={() => {
+    glide.update({
+      autoplay: 5000,
+    })
+  }}>
+    <div class="m-auto glide" use:mountGlide>
+      <div class="glide__track" data-glide-el="track">
+        <ul class="glide__slides">
+          {#each testimonios as t, i}
+            <li class="py-12 glide__slide">
+              <div class="flex flex-wrap-reverse items-center w-full mx-auto -m-12 text-xl md:flex-wrap font-title">
+                <div class="w-full h-full p-12 lg:w-1/2">
+                  <div class="flex flex-col w-full h-full">
+                    <p class="text-lg italic font-light">
+                    {t.cita}
+                    </p>
+                    <p class="mt-6 font-bold">
+                    {t.autor}
+                    </p>
+                  </div>
+                </div>
+                <div class="w-full px-12 lg:w-1/2">
+                  <div class="w-full overflow-hidden bg-gray-500 shadow-xl md:my-12 h-96 rounded-3xl">
+                    <img src="/images/{t.imagen}.webp" alt={t.autor} class="object-cover w-full h-full">
+                  </div>
+                </div>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <div class="flex w-full">
+        <div class="mx-auto glide__bullets" data-glide-el="controls[nav]">
+          {#each testimonios as _, i}
+            <button class="glide__bullet" data-glide-dir="={i}"></button>
+          {/each}
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="w-full p-12 lg:w-1/2">
-    <div class="w-full overflow-hidden bg-gray-500 shadow-2xl h-96 rounded-3xl a2" class:intersecting>
-      <img src="/images/testimonio.webp" alt="" class="object-cover w-full h-full">
-    </div>
-  </div>
-</V>
+  </V>
+</div>
