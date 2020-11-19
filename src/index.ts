c45de15784788c95f2c41c7a8e201e3215bc07ed
Juan Villacorta <juanvillacortac@gmail.com>
2020-11-19 13:58:06 -0400

    I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ import 'prismjs-tomorrow-theme/prism-tomorrow.css'
 import App from './App.svelte'
 import wf from 'webfontloader'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
-
 wf.load({
   google: {
     families: ['Oxygen:300,400', 'Ubuntu', 'Inconsolata']
   }
+})
+
+const app = new App({
+  target: document.getElementById('app'),
 })
 
 export default app
