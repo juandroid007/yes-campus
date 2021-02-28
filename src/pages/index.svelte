119ff10cb6dfa2176b4a1101639cfb99ca7389f8
Juan Villacorta <juanvillacortac@gmail.com>
2021-02-28 10:19:44 -0400

    Performance

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,22 +1,16 @@
 <script>
   import svitsConfig from '../../svits.config'
-  import { preferences } from '../stores/preferences'
   import { metatags } from '@roxi/routify'
   import Servicios from '../components/pages/home/Servicios.svelte'
   import PrimeraCapa from '../components/pages/home/Capa1.svelte'
   import SegundaCapa from '../components/pages/home/Capa2.svelte'
   import TerceraCapa from '../components/pages/home/Capa3.svelte'
   import CuartaCapa from '../components/pages/home/Capa4'
-  import SextaCapa from '../components/pages/home/Capa6.svelte'
   import ZonaOscura, { inZoneMask } from '../components/pages/home/ZonaOscura.svelte'
   import Programas from '../components/pages/home/Programas.svelte'
   import Cursos from '../components/pages/home/Cursos.svelte'
 
   import { fade } from 'svelte/transition'
-
-  // import { getCollection } from '../collections'
-
-  $: dark = $preferences.darkMode
 
   let outOffset
 
@@ -28,6 +22,8 @@
     @apply bg-yes-gray-800;
   }
 </style>
+
+<!-- routify:options preload=1 -->
 
 <div class="w-full min-h-screen">
   {#if $inZoneMask}
